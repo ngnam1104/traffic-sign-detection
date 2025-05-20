a5e0a2f66b061c88894457798a620662d58e0d9e
Nguyen Hai Nam <119506330+ngnam1104@users.noreply.github.com>
2025-05-20 20:19:01 +0700

    Update train_and_test.py

--- train_and_test.py
+++ train_and_test.py
@@ -20,7 +20,7 @@ def test_yolo(model_path, yaml_path, conf_threshold=0.25):
 if __name__ == "__main__":
     # ==== CẤU HÌNH ====
     train = True     # Đặt True để train, False để test
-    model_id = "traffic-sign-detection/best_enhanced_loss.pt"
+    model_id = "traffic-sign-detection/best.pt"
     yaml_path = "datasets/tt100k.yaml"
     epochs = 20
     batch_size = 16
